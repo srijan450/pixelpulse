da880362a23179ee0a84462c3f10f5d70c738cb8
srijan shankar dubey <srijanshankardubey99@gmail.com>
2023-09-01 21:04:39 +0530

    ci/cd testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p>Hello World</p>
       </div>
 
       <div className={styles.center}>
@@ -91,5 +71,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
